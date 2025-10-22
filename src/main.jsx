c038b59ce1c20d874db45f5b9c4dde4ca717f1d2
ahmed1349 <brillianta134@gmail.com>
2025-10-23 01:36:11 +0300

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { HashRouter } from 'react-router-dom'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <React.StrictMode>
     <Provider store={store}>
     <App />
     </Provider>
-  </HashRouter>,
+  </React.StrictMode>,
 )
